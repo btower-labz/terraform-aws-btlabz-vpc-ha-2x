38988cd6db887a47d295f559108409ea8b545d12
BTower Labz <labz@btower.net>
2020-08-01 15:18:57 +0000

    updated version

--- var-cidr.tf
+++ var-cidr.tf
@@ -1,29 +1,30 @@
 variable "vpc_cidr" {
   description = "VPC CIDR range."
-  type        = "string"
+  type        = string
   default     = "172.18.0.0/16"
 }
 
 variable "public_a_cidr" {
   description = "Public network A CIDR range."
-  type        = "string"
+  type        = string
   default     = "172.18.1.0/24"
 }
 
 variable "public_b_cidr" {
   description = "Public network B CIDR range."
-  type        = "string"
+  type        = string
   default     = "172.18.2.0/24"
 }
 
 variable "private_a_cidr" {
   description = "Private network A CIDR range."
-  type        = "string"
+  type        = string
   default     = "172.18.11.0/24"
 }
 
 variable "private_b_cidr" {
   description = "Private network B CIDR range."
-  type        = "string"
+  type        = string
   default     = "172.18.12.0/24"
 }
+
